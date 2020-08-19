d58328540c6651a30f60e6025f4ffe7ed1b65139
Catalina Patel <patelcatalina@gmail.com>
2020-08-18 22:40:46 -0400

    server.js updates

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,15 @@ const mysql = require('mysql');
 
 const local = {
     host: 'localhost',
-    port: process.env.PORT || 3306,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWD,
+    port: 3306,
+    user: "root",
+    password: "",
     database: 'burgers_db'
 };
 
 
-const jawsdb = process.env.DATABASE_URL;
-const connParams = (process.env.DATABASE_URL) ? jawsdb : local;
+const jawsdb = process.env.JAWSDB_URL;
+const connParams = (process.env.JAWSDB_URL) ? jawsdb : local;
 const connection = mysql.createConnection(connParams);
 
 
