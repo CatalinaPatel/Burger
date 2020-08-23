fbd1863159f898adba0cad9453e0226f521ecb09
Catalina Patel <patelcatalina@gmail.com>
2020-08-23 12:11:03 -0400

    updated burger_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,79 +1,40 @@
-'use strict';
+const express = require('express');
 
-// const express = require('express');
 const burger = require('../models/burger');
 
-class Router {
-    constructor(expressApp) {
-        this.app = expressApp;
+const router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+
+router.post("/api/burger", function (req, res) {
+    burger.create(req.body, function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: true,
+    }, condition, function (result) {
+
+        res.status(200).end();
     }
+    );
+});
 
-    start() {
-        this.index();
-        //this.add();
-        //this.devour();
-        //this.remove();
-    }
 
-    index() {
-        app.get('/', (req, res) => {
-            burger.list()
-                .then(burgers => {
-                    res.render('index', {
-                        burgers: burgers
-                    });
-                })
-                .catch(error => console.log(error));
-        });
-    }
 
-    add() {
-        app.post('/add', (req, res) => {
-            const burgerName = req.body.burgerName;
-
-            if (/^\W*$/.test(burgerName)) {
-                console.log('No empty burger name allowed');
-            }
-            else {
-                burger.add(burgerName)
-                    .then(result => {
-                        res.redirect('/');
-                    })
-                    .catch(error => console.log(error));
-            }
-        });
-    }
-
-    devour() {
-        app.put('/devour/:id', (req, res) => {
-            // Convert "true" string to 1 (boolean)
-            if ('devoured' in req.body && req.body['devoured'] === 'true') {
-                req.body['devoured'] = 1;
-            }
-
-            burger.devour(parseInt(req.params.id), req.body)
-                .then(result => {
-                    res.redirect('/');
-                })
-                .catch(error => {
-                    console.log(error);
-                    res.redirect('/');
-                });
-        });
-    }
-
-    remove() {
-        app.delete('/remove/:id', (req, res) => {
-            burger.delete(parseInt(req.params.id))
-                .then(result => {
-                    res.redirect('/');
-                })
-                .catch(error => {
-                    console.log(error);
-                    res.redirect('/');
-                });
-        });
-    }
-}
-
-module.exports = Router;
+module.exports = router;
